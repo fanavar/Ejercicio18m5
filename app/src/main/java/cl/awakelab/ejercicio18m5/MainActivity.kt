85af8da52fec3c68f037d0298b2e6ccb8ca87d9c
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-21 20:34:07 -0400

    boton guardar con set on click listener

--- app/src/main/java/cl/awakelab/ejercicio18m5/MainActivity.kt
+++ app/src/main/java/cl/awakelab/ejercicio18m5/MainActivity.kt
@@ -5,9 +5,21 @@ import android.os.Bundle
 import cl.awakelab.ejercicio18m5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnGuardar.setOnClickListener{
+            var texto = binding.ingresarTexto.text.toString()
+            var entero = binding.valorEntero.text.toString().toInt()
+            var decimal = binding.ingresarDecimal.text.toString().toDouble()
+            var switch = binding.switch1.isChecked
+        }
+
+        binding.btnBorrar.setOnClickListener{
+
+        }
     }
 }
